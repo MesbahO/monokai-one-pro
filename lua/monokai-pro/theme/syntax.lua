5c42479dba56411a3fd5ed8daf463261af09c353
Mesbah <mesbah.omid@gmail.com>
2023-06-25 13:36:07 -0700

    syntac colour update

--- lua/monokai-pro/theme/syntax.lua
+++ lua/monokai-pro/theme/syntax.lua
@@ -6,15 +6,15 @@ M.setup = function(c, config, _)
   local styles = config.styles
   return {
     Comment = { fg = c.base.dimmed3, italic = styles.comment.italic }, -- Comments
-    Constant = { fg = c.base.magenta }, -- (preferred) any constant
-    String = { fg = c.base.yellow }, --   a string constant: "this is a string"
+    Constant = { fg = c.base.white }, -- (preferred) any constant
+    String = { fg = c.base.green}, --   a string constant: "this is a string"
     Character = { fg = c.base.magenta }, -- a character constant: 'c', '\n'
-    Number = { fg = c.base.magenta }, -- a number constant: 234, 0xff
-    Boolean = { fg = c.base.magenta }, -- a boolean constant: TRUE, false
-    Float = { fg = c.base.magenta }, -- a floating point constant: 2.3e10
+    Number = { fg = c.base.blue}, -- a number constant: 234, 0xff
+    Boolean = { fg = c.base.blue}, -- a boolean constant: TRUE, false
+    Float = { fg = c.base.blue }, -- a floating point constant: 2.3e10
     Identifier = { fg = c.base.white }, -- (preferred) any variable name
-    Function = { fg = c.base.green }, -- function name (also: methods for classes)
-    Statement = { fg = c.base.magenta }, -- (preferred) any statement
+    Function = { fg = c.base.magenta }, -- function name (also: methods for classes)
+    Statement = { fg = c.base.white }, -- (preferred) any statement
     Conditional = { fg = c.base.red }, --  if, then, else, endif, switch, etc
     Repeat = { fg = c.base.red }, -- for, do, while, etc
     Label = { fg = c.base.red }, -- case, default, etc
@@ -26,7 +26,7 @@ M.setup = function(c, config, _)
     Define = { fg = c.base.red }, -- preprocessor #define
     Macro = { fg = c.base.red }, -- same as Define
     PreCondit = { fg = c.base.red }, -- preprocessor #if, #else, #endif, etc
-    Type = { fg = c.base.white }, -- React, ReactDOM (import React from 'react';)
+    Type = { fg = c.base.yellow }, -- React, ReactDOM (import React from 'react';)
     StorageClass = { fg = c.base.red, italic = styles.type.italic }, -- static, register, volatile, etc
     Structure = { fg = c.base.cyan, italic = styles.type.italic }, -- struct, union, enum, etc
     Typedef = { fg = c.base.red }, -- A typedef
